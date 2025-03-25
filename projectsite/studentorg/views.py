ed5f1f5436d4f04335303c29e6623fb41e01fc57
Urthemang <lmslkatana@gmail.com>
2025-03-25 19:44:16 +0800

    Modifications on urls.py, views.py updates onn org_edit and org_list

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
@@ -12,13 +12,19 @@ class HomePageView(ListView):
 	template_name = "home.html"
  
 class OrganizationList(ListView):
-	model = Organization
-	context_object_name = 'organization'
-	template_name = 'org_list.html'
-	paginate_by = 5
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'organizations.html'
+    paginate_by = 5
  
 class OrganizationCreateView(CreateView):
-	model = Organization
-	form_class = OrganizationForm
-	template_name = 'org_add.html'
-	success_url = reverse_lazy('organization-list')
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'organization_add.html'
+    success_url = reverse_lazy('organization-list')
+ 
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'organization_edit.html'
+    success_url = reverse_lazy('organization-list')
